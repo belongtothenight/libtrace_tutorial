b7989e3a2a0e1f96d14bcf178e0bd7582e6534bf
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-05-02 14:14:49 +0800

    Copy files from ACN_Code/hw4_libtrace_setup

---
+++ setup/remove.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+#
+# Packages installed using dpkg/apt are not removed
+
+# ====================================================================================
+# Init
+# ====================================================================================
+bash_function_file="./functions.sh"
+
+echo "Sourcing bash functions"
+source "${bash_function_file}" || { echo 'source failed'; exit 1; } # Enter fail-exit mode
+source "./common_functions.sh"
+load_preset "./config.ini"
+
+# ====================================================================================
+# Backup files
+# ====================================================================================
+msg="backup"
+if [ $task_uthash == 1 ] || [ $task_libwandder == 1 ] || [ $task_wandio == 1 ] || [ $task_libtrace == 1 ]; then
+    echo_notice "$this_script" "$msg" "Backing up files"
+    sudo rm -rf "${program_install_dir}/backup_include"
+    sudo cp -rf "${system_include_dir}/" "${program_install_dir}/"
+    sudo mv "${program_install_dir}/include/" "${program_install_dir}/backup_include/"
+    sudo rm -rf "${program_install_dir}/backup_lib"
+    sudo cp -rf "${system_lib_dir}/" "${program_install_dir}/"
+    sudo mv "${program_install_dir}/lib/" "${program_install_dir}/backup_lib/"
+fi
+
+# ====================================================================================
+# Remove uthash
+# ====================================================================================
+msg="remove uthash"
+if [ $task_uthash == 1 ]; then
+    echo_notice "$this_script" "$msg" "Removing uthash"
+    sudo rm -rf "${program_install_dir}/${uthash_name}"
+    sudo rm -f ${system_include_dir}/ut*.h
+fi
+
+# ====================================================================================
+# Remove libwandder
+# ====================================================================================
+msg="remove libwandder"
+if [ $task_libwandder == 1 ]; then
+    echo_notice "$this_script" "$msg" "Removing libwandder"
+    sudo rm -f ${system_include_dir}/libwandder*.h
+    sudo rm -f ${system_lib_dir}/libwandder*
+    sudo rm -f "${program_install_dir}/${libwandder_file}"
+    sudo rm -rf "${program_install_dir}/${libwandder_ln}"
+    sudo rm -f "${program_install_dir}/${libwandder_name}"
+fi
+
+# ====================================================================================
+# Remove wandio
+# ====================================================================================
+msg="remove wandio"
+if [ $task_wandio == 1 ]; then
+    echo_notice "$this_script" "$msg" "Removing wandio"
+    sudo rm -f ${system_include_dir}/wandio*.h
+    sudo rm -f ${system_lib_dir}/libwandio*
+    sudo rm -f "${program_install_dir}/${wandio_file}"
+    sudo rm -rf "${program_install_dir}/${wandio_ln}"
+    sudo rm -f "${program_install_dir}/${wandio_name}"
+fi
+
+# ====================================================================================
+# Remove libtrace
+# ====================================================================================
+msg="remove libtrace"
+if [ $task_libtrace == 1 ]; then
+    echo_notice "$this_script" "$msg" "Removing libtrace"
+    sudo rm -f ${system_include_dir}/libtrace*.h
+    sudo rm -f ${system_include_dir}/libpacketdump*.h
+    sudo rm -rf "${system_include_dir}/${libtrace_name}"
+    sudo rm -f ${system_lib_dir}/libtrace*
+    sudo rm -rf ${system_lib_dir}/libpacketdump*
+    sudo rm -f "${program_install_dir}/${libtrace_file}"
+    sudo rm -rf "${program_install_dir}/${libtrace_ln}"
+    sudo rm -f "${program_install_dir}/${libtrace_name}"
+fi
+
+# ====================================================================================
+# Remove libtrace tutorial repo
+# ====================================================================================
+msg="remove libtrace tutorial"
+if [ $task_libtrace_tutorial == 1 ]; then
+    echo_notice "$this_script" "$msg" "Removing libtrace tutorial repo"
+    sudo rm -rf "${program_install_dir}/libtrace_tutorial"
+fi
+
+# End of file
+msg="cleanup"
+echo_notice "$this_script" "$msg" "Cleanup complete"
