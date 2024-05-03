fa4176e87943386d1b1ef55fbe5d1ac4097cb505
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-05-04 01:54:36 +0800

    [Severity: Low] Move param value check to common_functions.sh with correct function usage syntax
    Commits from source:
    1. ab14cc6228c8b78e134e07fe5e232ab6b0f1770f
    2. 5a6a454cbb10726f18a586cb45d1122946639c82
    
    Solves:
    1. Multiple redundant param value check.
    2. Misuse bash function "echo_error".
    3. Echo messages hard to see before custom function loads.

--- setup/common_functions.sh
+++ setup/common_functions.sh
@@ -29,6 +29,15 @@ load_preset () {
     check_var script_stat                   $error_exit_code
 
     if [ $verbose == 1 ]; then
+        echo_notice "$message1" "$message2" "Checking variable values"
+    fi
+    #[ PARAM - TASKS ] X 5(skip)
+    #[ PARAM - EXECUTION MODE ] X 1
+    if [ $script_stat != "dev" ] && [ $script_stat != "prod" ]; then
+        echo_error "$message1" "$message2" "Invalid script_stat: should be either dev or prod" $error_exit_code
+    fi
+
+    if [ $verbose == 1 ]; then
         echo_notice "$message1" "$message2" "Setting private variables"
     fi
     wget_flags="-nv --show-progress"
@@ -55,9 +64,6 @@ load_preset () {
         :
     elif [ $script_stat == "prod" ]; then
         make_flags="-j$(nproc)"
-    else
-        echo_error "$message1" "$message2" "Invalid script_stat"
-        exit $error_exit_code
     fi
     libwandder_file="${libwandder_name}-${libwandder_release_url##*/}"
     libwandder_ln="${libwandder_file//.tar.gz}"
